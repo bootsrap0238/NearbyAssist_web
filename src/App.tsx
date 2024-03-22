83ff82677f42a947b54447502b1a52555e539487
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-22 20:33:39 +0800

    feat: temporary homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import ExampleContextProvider from "./context/example_context";
 import ExamplePage from "./pages/example_page";
+import HomePage from "./pages/home_page";
 
 export default function App() {
     return (
         <ExampleContextProvider>
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/example" element={<ExamplePage />} />
             </Routes>
         </ExampleContextProvider>
