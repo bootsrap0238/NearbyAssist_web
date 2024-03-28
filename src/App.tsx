c6dd627ec4a16baa9c6736156d5459b9551a5fc3
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-28 20:40:36 +0800

    feat: main layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,32 @@ import VendorApplication from "./pages/vendor_application";
 import NotFound from "./pages/not_found";
 import AccountRestriction from "./pages/account_restriction";
 import AccountManagement from "./pages/account_management";
+import MainLayout from "./layout/MainLayout";
 
 export default function App() {
     return (
         <ExampleContextProvider>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/complaints" element={<Complaints />} />
-                <Route
-                    path="/vendor_application"
-                    element={<VendorApplication />}
-                />
-                <Route
-                    path="/account_restriction"
-                    element={<AccountRestriction />}
-                />
-                <Route
-                    path="/account_management"
-                    element={<AccountManagement />}
-                />
-                <Route path="/example" element={<ExamplePage />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/complaints" element={<Complaints />} />
+                    <Route
+                        path="/vendor_application"
+                        element={<VendorApplication />}
+                    />
+                    <Route
+                        path="/account_restriction"
+                        element={<AccountRestriction />}
+                    />
+                    <Route
+                        path="/account_management"
+                        element={<AccountManagement />}
+                    />
+                    <Route path="/example" element={<ExamplePage />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
             </Routes>
         </ExampleContextProvider>
     );
