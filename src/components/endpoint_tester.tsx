678009e48140bfe9be32f344e66db1aab931afa1
John Loyd Mulit <jlmulit68@gmail.com>
2024-06-26 15:59:59 +0800

    feat: endpoint tester in example page

---
+++ src/components/endpoint_tester.tsx
@@ -0,0 +1,77 @@
+import { useRef, useState } from "react";
+import useRequest from "../hooks/use_request";
+import useStorage from "../hooks/use_storage";
+
+export default function EndpointTester() {
+    const endpointRef = useRef<HTMLInputElement>(null);
+    const [result, setResult] = useState("test result goes here...");
+
+    const { send, isLoading } = useRequest();
+    const { getSavedUser } = useStorage();
+
+    async function testEndpoint() {
+        if (!endpointRef.current) {
+            setResult("endpoint input is undefined");
+            return;
+        }
+
+        if (endpointRef.current.value === "") {
+            setResult("url cannot be empty");
+            return;
+        }
+
+        const url = `${import.meta.env.VITE_BACKEND_URL}${endpointRef.current.value}`;
+
+        const user = getSavedUser();
+        if (user === null) {
+            setResult("no user data found");
+            return;
+        }
+
+        try {
+            const response = await send(user.accessToken, url, "GET");
+            console.log(response);
+            if (!response.success) {
+                setResult(response.error);
+                return;
+            }
+
+            setResult(JSON.stringify(response.data, null, "\t"));
+        } catch (e) {
+            setResult(JSON.stringify(e, null, "\t"));
+        }
+    }
+
+    return (
+        <div className="grid gap-2 rounded p-2 outline outline-1 outline-gray-lightgray">
+            <h2>Endpoint Tester</h2>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+
+                if (isLoading) {
+                    return;
+                }
+
+                testEndpoint();
+            }}
+                className="grid gap-2"
+            >
+                <input
+                    type="text"
+                    placeholder="endpoint"
+                    ref={endpointRef}
+                    className="rounded p-1 outline outline-1 outline-gray-lightgray"
+                />
+                <div className="flex justify-start">
+                    <button
+                        type="submit"
+                        className="cursor rounded bg-primary p-2"
+                    >{isLoading ? "loading..." : "send request"}</button>
+                </div>
+            </form>
+            <div className="p-1 outline outline-1 outline-gray-lightgray">
+                <pre>{result}</pre>
+            </div>
+        </div>
+    )
+}
--- src/pages/example_page.tsx
+++ src/pages/example_page.tsx
@@ -3,38 +3,15 @@ import ExampleButton from "../components/example_button";
 import useAuth from "../hooks/use_auth";
 import UseExampleContext from "../hooks/use_example_context";
 import useRequest from "../hooks/use_request";
-import useStorage from "../hooks/use_storage";
+import EndpointTester from "../components/endpoint_tester";
 
 export default function ExamplePage() {
     const { value } = UseExampleContext();
     const navigate = useNavigate();
 
     const { logout } = useAuth();
-    const { send, isLoading } = useRequest();
-    const { getSavedUser } = useStorage();
+    const { isLoading } = useRequest();
 
-    async function sample() {
-        const serverAddr = import.meta.env.VITE_BACKEND_URL;
-        const url = `${serverAddr}/v1/health`;
-
-        const user = getSavedUser();
-        if (user === null) {
-            console.log("no user data found");
-            return;
-        }
-
-        try {
-            const response = await send(user.accessToken, url, "GET");
-            if (!response.success) {
-                console.log("Request failed");
-                return;
-            }
-
-            console.log(response.data);
-        } catch (e) {
-            console.error(e);
-        }
-    }
 
     async function sampleLogout() {
         try {
@@ -50,20 +27,18 @@ export default function ExamplePage() {
     }
 
     return (
-        <div>
+        <div className="grid gap-2 p-2">
             <h1 className="my-2">This is from example page</h1>
             <p className="my-2">context value: {value}</p>
-            <ExampleButton
-                title={isLoading ? "loading..." : "test request"}
-                action={sample}
-                styles="rounded bg-primary p-2"
-            />
-            <br />
-            <ExampleButton
-                title={isLoading ? "loading..." : "test logout"}
-                action={sampleLogout}
-                styles="rounded bg-red-lightred p-2 text-white"
-            />
+            <div className="flex gap-2">
+                <ExampleButton
+                    title={isLoading ? "loading..." : "test logout"}
+                    action={sampleLogout}
+                    styles="rounded bg-red-lightred p-2 text-white"
+                />
+            </div>
+
+            <EndpointTester />
         </div>
     );
 }
